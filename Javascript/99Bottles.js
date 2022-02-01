596d8b9a451a160dd3bc63d1f70d97aa2612c7c9
Will Spedding-Jones <willsjones95@gmail.com>
2022-02-01 15:57:01 +0000

    tasks completed

---
+++ Javascript/99Bottles.js
@@ -0,0 +1,9 @@
+`strict mode`
+for (let i = 99; i>=0; i--) {
+    if (i > 0) {
+        console.log(`${i} bottles of beer on the wall, ${i} bottles of beer. Take one down and pass it around, ${i-1} bottles of beer on the wall.`)
+    } else { 
+        i == 0
+        console.log(`No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.`)
+    }
+}
