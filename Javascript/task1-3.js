92b165da471b051cc37191f7983d7346b2827ffc
Will Spedding-Jones <willsjones95@gmail.com>
2022-02-01 10:39:09 +0000

    Java tasks

---
+++ Javascript/task1-3.js
@@ -0,0 +1,18 @@
+'strict mode';
+
+console.log("Will");
+
+console.info("SJ");
+
+console.warn("UK");
+
+console.error("Aires");
+
+let favcar = "Zonda"
+let favmod = "Pagani"
+
+console.log("My favourite car is " + favmod + " and the model is " + favcar)
+console.log('My favourite car is ${favmod} and the model is ${favcar} ');
+console.log("")
+
+console.log("This is not changed %c but this is.","color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
