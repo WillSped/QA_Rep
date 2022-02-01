8c88e71549f0f8d51576e74120ea3a4b7d22d9ce
Will Spedding-Jones <willsjones95@gmail.com>
2022-02-01 10:46:50 +0000

    edited

--- Javascript/task1-3.js
+++ Javascript/task1-3.js
@@ -12,7 +12,6 @@ let favcar = "Zonda"
 let favmod = "Pagani"
 
 console.log("My favourite car is " + favmod + " and the model is " + favcar)
-console.log('My favourite car is ${favmod} and the model is ${favcar} ');
-console.log("")
+console.log(`%c My favourite car is ${favmod} and the model is ${favcar}`);
 
-console.log("This is not changed %c but this is.","color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
+console.log("%c Wow look at this amazing text","color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
